d017faf0ed431ad775c22da088bd2e5432cdae14
walrashoud <iwaleed@gmail.com>
2021-04-18 20:50:22 +0300

    adding contact-submit api for contact us form

---
+++ NextMasjid.Backend.API/Controllers/ContactController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using NextMasjid.Backend.Core.Data;
+using NextMasjid.Backend.API.Models;
+
+namespace NextMasjid.Backend.API.Controllers
+{
+    [ApiController]
+    [Route("contact")]
+    public class ContactConttroller : BaseController<ContactConttroller>
+    {
+        [HttpPost("submit")]
+        public IActionResult Submit([FromForm] ContactModel contact)
+        {
+            // todo send email
+            return CreatedAtAction("", new { });
+        }
+    }
+
+    public class ContactModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Content { get; set; }
+    }
+}
